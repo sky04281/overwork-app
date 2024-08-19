e3a32952ff24f1532fce64e2733f3a71ff5937d3
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-08-19 20:14:33 +0800

    title

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import {
-    SafeAreaView,
     View,
     Text,
     Button,
     TouchableOpacity,
     Pressable,
+    ScrollView,
 } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import { useRouter, Link } from 'expo-router'
 
@@ -13,20 +14,20 @@ const LoginScreen = () => {
     const router = useRouter()
 
     return (
-        <SafeAreaView>
-            <View className="flex flex-row justify-center">
-                {/* <Link className="w-10 h-5 dark:text-white" href={'/(tabs)'}>
-                    登入
-                </Link> */}
-                <Pressable
-                    className="bg-blue-500 h-10 w-20 rounded-lg flex flex-row justify-center items-center"
-                    onPress={() => {
-                        router.push('/(tabs)')
-                    }}
-                >
-                    <Text className="dark:text-white">登入</Text>
-                </Pressable>
-            </View>
+        <SafeAreaView className="h-full">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="w-full h-full justify-center items-center">
+                    <Text className="text-4xl dark:text-white">過負荷APP</Text>
+                    <Pressable
+                        className="bg-blue-500 h-10 w-20 rounded-lg flex flex-row justify-center items-center"
+                        onPress={() => {
+                            router.push('/(tabs)')
+                        }}
+                    >
+                        <Text className="dark:text-white">登入</Text>
+                    </Pressable>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
