a9e5fdb6aa60b851c406a6a281656276d2eedadc
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-08-24 16:46:35 +0800

    style: set to default theme
    remove classname dark:

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                tabBarActiveTintColor: Colors['light'].tint,
                 headerShown: false,
             }}
         >
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,9 +31,7 @@ export default function RootLayout() {
     }
 
     return (
-        <ThemeProvider
-            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
+        <ThemeProvider value={DefaultTheme}>
             <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             </Stack>
