674ebe5fcf169e64d2e73807d2677b3c733ba7c2
KevinChen <120458845+KevinLaKao@users.noreply.github.com>
2024-08-25 19:52:54 +0800

    feat:健康儀表板頁面版本1st (#5)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,77 +4,196 @@ import {
     SafeAreaView,
     StyleSheet,
     ScrollView,
+    Image,
     FlatList,
+    Pressable,
 } from 'react-native'
+import AntDesign from '@expo/vector-icons/AntDesign'
+import { useState } from 'react'
+import InfoModal from '@/components/InfoModal'
 
 export default function HomeScreen() {
+    const [infoModalVisible, setInfoModalVisible] = useState(false)
+    const RESULT = { style: 'text-red-500', level: '高風險' }
+    const TITLE = '疾病一'
+    const INFO =
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat alias aspernatur natus architecto non magnam ipsum placeat accusantium harum pariatur, deserunt cupiditate? Voluptate dignissimos laudantium dolorum quidem, repudiandae maiores sint.'
     const DATA = [
         {
-            name: '疾病一',
-            info: 'infoinfoinfoinfoinfoinfo',
+            name: TITLE,
+            info: INFO,
         },
         {
-            name: '疾病二',
-            info: 'infoinfoinfoinfoinfoinfo',
+            name: TITLE,
+            info: INFO,
         },
         {
-            name: '疾病三',
-            info: 'infoinfoinfoinfoinfoinfo',
+            name: TITLE,
+            info: INFO,
         },
         {
-            name: '疾病四',
-            info: 'infoinfoinfoinfoinfoinfo',
+            name: TITLE,
+            info: INFO,
         },
         {
-            name: '疾病五',
-            info: 'infoinfoinfoinfoinfoinfo',
+            name: TITLE,
+            info: INFO,
         },
     ]
-
+    const handlePressInfo = () => {
+        setInfoModalVisible(true)
+    }
     return (
         <SafeAreaView>
-            <ScrollView className="">
-                <View className="flex justify-center items-center h-[5vh]">
-                    <Text className="text-xl font-bold">過負荷評量</Text>
-                </View>
-                <View className="flex flex-col">
-                    <View className="flex justify-center items-center h-[7.5vh] mt-[2vh]">
-                        <Text>等級用光譜顯示</Text>
-                    </View>
-                    <View className="flex justify-center items-center h-[7.5vh]">
-                        <Text>過負荷評分</Text>
-                    </View>
-                </View>
-                <View className="flex-1 flex-col justify-center items-center">
-                    <View className="flex justify-center items-center h-[5vh] mt-[5vh]">
-                        <Text className="text-xl font-bold">
-                            高風險潛在疾病
+            <ScrollView>
+                <View
+                    className={
+                        infoModalVisible
+                            ? 'bg-gray-400 opacity-40 ease-in-out'
+                            : 'ease-in-out'
+                    }
+                >
+                    <View className="flex justify-center items-center h-[5vh]">
+                        <Text className="text-xl font-semibold">
+                            過負荷評量
                         </Text>
                     </View>
-                    <View className="flex flex-row items-center h-[25vh] w-[90vw]">
-                        <FlatList
-                            horizontal
-                            data={DATA}
-                            renderItem={({ item }) => (
-                                <View className="flex flex-col justify-center items-center h-[15vh] w-[55vw] mr-[5vw] mb-[2.5vh] border border-solid rounded-[15px]">
-                                    <Text className="text-lg mb-[2vh]">
-                                        {item.name}
-                                    </Text>
-                                    <Text className="">{item.info}</Text>
-                                </View>
-                            )}
-                        />
+                    <View className="flex">
+                        <View className="flex justify-center items-center h-[10vh] my-[2vh]">
+                            <Image
+                                className="scale-[0.3]"
+                                source={require('../../assets/images/progressbar.png')}
+                            />
+                        </View>
+                        <View className="flex justify-center items-center">
+                            <Text className="text-sm font-bold h-[3vh]">
+                                您的過負荷評分為 :{' '}
+                                <Text className={RESULT.style}>
+                                    {RESULT.level}
+                                </Text>
+                            </Text>
+                            <Text className="text-xs">
+                                根據您的過負荷評分，提供以下資訊參考
+                            </Text>
+                            <Text className="text-xs">
+                                若您的過負荷評分風險較高，請儘速尋求專業醫療協助
+                            </Text>
+                        </View>
+                    </View>
+                    <View className="flex-1 justify-center items-center">
+                        <View className="flex justify-center items-center h-[5vh] mt-[5vh]">
+                            <Text className="text-xl font-semibold">
+                                高風險潛在疾病
+                            </Text>
+                        </View>
+                        <View className="flex flex-row items-center h-[25vh] w-[90vw]">
+                            <FlatList
+                                horizontal
+                                data={DATA}
+                                renderItem={({ item }) => (
+                                    <View className="flex h-[15vh] w-[55vw] mr-[5vw] mb-[2.5vh] border border-solid rounded-[15px]">
+                                        <View className="flex flex-row justify-between items-center ml-[3vw] mr-[4vw] mt-[1vh]">
+                                            <Text className="text-base font-semibold text-sky-700">
+                                                {item.name}
+                                            </Text>
+                                            <Pressable
+                                                onPress={handlePressInfo}
+                                            >
+                                                <AntDesign
+                                                    name="infocirlceo"
+                                                    size={16}
+                                                    color="black"
+                                                />
+                                            </Pressable>
+                                        </View>
+                                        <View className="flex justify-start mx-[3vw] my-[0.5vh]">
+                                            <Text className="text-sky-900 h-[10vh]">
+                                                {item.info}
+                                            </Text>
+                                        </View>
+                                    </View>
+                                )}
+                            />
+                        </View>
+                    </View>
+                    <View className="flex justify-center items-center h-[5vh]">
+                        <Text className="text-xl font-semibold">衛教資訊</Text>
+                    </View>
+                    <View className="flex justify-center items-center mt-[5vh]">
+                        <View className="flex h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]">
+                            <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
+                                <Text className="text-xl font-semibold text-indigo-700">
+                                    {TITLE}
+                                </Text>
+                                <Pressable onPress={handlePressInfo}>
+                                    <AntDesign
+                                        name="infocirlceo"
+                                        size={16}
+                                        color="black"
+                                    />
+                                </Pressable>
+                            </View>
+                            <View className="flex justify-start mx-[5vw] mt-[0.5vh] h-[8vh]">
+                                <Text className="text-indigo-900">{INFO}</Text>
+                            </View>
+                        </View>
+                        <View className="flex h-[15vh] w-[80vw] mb-[5vh] ml-[5vh] border border-solid rounded-[15px]">
+                            <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
+                                <Pressable onPress={handlePressInfo}>
+                                    <AntDesign
+                                        name="infocirlceo"
+                                        size={16}
+                                        color="black"
+                                    />
+                                </Pressable>
+                                <Text className="text-xl font-semibold text-indigo-700">
+                                    {TITLE}
+                                </Text>
+                            </View>
+                            <View className="flex justify-start mx-[5vw] mt-[0.5vh] h-[8vh]">
+                                <Text className="text-indigo-900">{INFO}</Text>
+                            </View>
+                        </View>
+                        <View className="flex h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]">
+                            <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
+                                <Text className="text-xl font-semibold text-indigo-700">
+                                    {TITLE}
+                                </Text>
+                                <Pressable onPress={handlePressInfo}>
+                                    <AntDesign
+                                        name="infocirlceo"
+                                        size={16}
+                                        color="black"
+                                    />
+                                </Pressable>
+                            </View>
+                            <View className="flex justify-start mx-[5vw] mt-[0.5vh] h-[8vh]">
+                                <Text className="text-indigo-900">{INFO}</Text>
+                            </View>
+                        </View>
+                        <View className="flex h-[15vh] w-[80vw] mb-[5vh] ml-[5vh] border border-solid rounded-[15px]">
+                            <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
+                                <Pressable onPress={handlePressInfo}>
+                                    <AntDesign
+                                        name="infocirlceo"
+                                        size={16}
+                                        color="black"
+                                    />
+                                </Pressable>
+                                <Text className="text-xl font-semibold text-indigo-700">
+                                    {TITLE}
+                                </Text>
+                            </View>
+                            <View className="flex mx-[5vw] mt-[0.5vh] h-[8vh]">
+                                <Text className="text-indigo-900">{INFO}</Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
-                <View className="flex justify-center items-center h-[5vh]">
-                    <Text className="text-xl font-bold">衛教資訊</Text>
-                </View>
-                <View className="flex flex-col justify-center items-center mt-[5vh]">
-                    <View className="flex justify-center items-center h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]"></View>
-                    <View className="flex justify-center items-center h-[15vh] w-[80vw] mb-[5vh] ml-[5vh] border border-solid rounded-[15px]"></View>
-                    <View className="flex justify-center items-center h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]"></View>
-                    <View className="flex justify-center items-center h-[15vh] w-[80vw] mb-[5vh] ml-[5vh] border border-solid rounded-[15px]"></View>
-                </View>
+                <InfoModal
+                    infoModalVisible={infoModalVisible}
+                    setInfoModalVisible={setInfoModalVisible}
+                />
             </ScrollView>
         </SafeAreaView>
     )
