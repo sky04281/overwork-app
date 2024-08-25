e395430e2f07af763279a6224290acad8841b133
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-08-25 19:47:08 +0800

    feat: body info Screen v1
    1. bodyInfoListItem：每筆生理資訊，資料用props傳

--- app/(tabs)/bodyInfoScreen.tsx
+++ app/(tabs)/bodyInfoScreen.tsx
@@ -1,10 +1,42 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { FlatList, SafeAreaView, ScrollView, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import Header from '@/components/tabs/header'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import BodyInfoListItem from '@/components/tabs/bodyInfoListItem'
 
 const BodyInfoScreen = () => {
+    const [bodyInfoList, setBodyInfoList] = React.useState<{ key: string }[]>(
+        []
+    )
+    useEffect(() => {
+        // fetch body info list
+        setBodyInfoList([
+            { key: '2024-08-23' },
+            { key: '2024-08-24' },
+            { key: '2024-08-25' },
+            { key: '2024-08-26' },
+            { key: '2024-08-27' },
+        ])
+    }, [])
     return (
-        <SafeAreaView>
-            <Text className="dark:text-white">生理資訊接收介面</Text>
+        <SafeAreaView className="h-full">
+            <Header title="生理資訊" />
+            <View className="w-full h-full justify-center">
+                <View className="w-full flex-row justify-between items-center mt-5 px-2 border-b-[2rem] border-black">
+                    <Text className="text-xl font-medium">
+                        個人生理資訊紀錄
+                    </Text>
+                    <FontAwesome name="plus" size={28} color="black" />
+                </View>
+                <View className="flex-1 items-start justify-start p-2">
+                    <FlatList
+                        data={bodyInfoList}
+                        renderItem={({ item }) => (
+                            <BodyInfoListItem title={item.key} />
+                        )}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
---
+++ components/tabs/bodyInfoListItem.tsx
@@ -0,0 +1,37 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign'
+
+interface BodyInfoListItemProps {
+    title: string
+    className?: string
+}
+
+const BodyInfoListItem = ({ ...props }: BodyInfoListItemProps) => {
+    const [show, setShow] = React.useState(false)
+    return (
+        <View>
+            <TouchableOpacity onPress={() => setShow(!show)}>
+                <View className="flex-row w-[95vw] items-center justify-between space-x-1 border-b-[1px] pt-1">
+                    <Text className="text-lg">{props.title}</Text>
+                    <AntDesign
+                        name={show ? 'caretup' : 'caretdown'}
+                        size={16}
+                        color="black"
+                        onPress={() => setShow(!show)}
+                    />
+                </View>
+                {show && (
+                    <View className="flex-col items-start space-y-1">
+                        <Text className="text-base">平均心率: 100 bpm</Text>
+                        <Text className="text-base">血壓: 120.80 mmHg</Text>
+                        <Text className="text-base">步數: 10,000 steps</Text>
+                        <Text className="text-base">睡眠時長: 6 hr</Text>
+                    </View>
+                )}
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default BodyInfoListItem
