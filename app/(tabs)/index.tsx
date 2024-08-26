eadfc5776aa0ff061af03d696662ffafb21da994
KevinChen <120458845+KevinLaKao@users.noreply.github.com>
2024-08-26 23:16:37 +0800

    先符合開會要求然後電池外觀完成邏輯還沒

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,13 +4,12 @@ import {
     SafeAreaView,
     StyleSheet,
     ScrollView,
-    Image,
-    FlatList,
     Pressable,
 } from 'react-native'
 import AntDesign from '@expo/vector-icons/AntDesign'
 import { useState } from 'react'
 import InfoModal from '@/components/InfoModal'
+import Battery from '@/components/RatingBattery'
 
 export default function HomeScreen() {
     const [infoModalVisible, setInfoModalVisible] = useState(false)
@@ -18,31 +17,10 @@ export default function HomeScreen() {
     const TITLE = '疾病一'
     const INFO =
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat alias aspernatur natus architecto non magnam ipsum placeat accusantium harum pariatur, deserunt cupiditate? Voluptate dignissimos laudantium dolorum quidem, repudiandae maiores sint.'
-    const DATA = [
-        {
-            name: TITLE,
-            info: INFO,
-        },
-        {
-            name: TITLE,
-            info: INFO,
-        },
-        {
-            name: TITLE,
-            info: INFO,
-        },
-        {
-            name: TITLE,
-            info: INFO,
-        },
-        {
-            name: TITLE,
-            info: INFO,
-        },
-    ]
     const handlePressInfo = () => {
         setInfoModalVisible(true)
     }
+
     return (
         <SafeAreaView>
             <ScrollView>
@@ -58,68 +36,34 @@ export default function HomeScreen() {
                             過負荷評量
                         </Text>
                     </View>
-                    <View className="flex">
-                        <View className="flex justify-center items-center h-[10vh] my-[2vh]">
-                            <Image
-                                className="scale-[0.3]"
-                                source={require('../../assets/images/progressbar.png')}
-                            />
+                    <View className="flex flex-row">
+                        <View className="flex flex-row justify-center items-center h-[35vh] w-[50vw]">
+                            <View className="flex justify-between items-center h-[25vh] w-[10vw] ml-[5vw]">
+                                <Text className="text-red-600">High</Text>
+                                <Text className="text-green-600">Low</Text>
+                            </View>
+                            <Battery />
                         </View>
-                        <View className="flex justify-center items-center">
-                            <Text className="text-sm font-bold h-[3vh]">
-                                您的過負荷評分為 :{' '}
-                                <Text className={RESULT.style}>
-                                    {RESULT.level}
-                                </Text>
-                            </Text>
-                            <Text className="text-xs">
-                                根據您的過負荷評分，提供以下資訊參考
-                            </Text>
-                            <Text className="text-xs">
-                                若您的過負荷評分風險較高，請儘速尋求專業醫療協助
-                            </Text>
+                        <View className="h-[35vh] w-[50vw]">
+                            <Text>right</Text>
                         </View>
                     </View>
-                    <View className="flex-1 justify-center items-center">
-                        <View className="flex justify-center items-center h-[5vh] mt-[5vh]">
-                            <Text className="text-xl font-semibold">
-                                高風險潛在疾病
-                            </Text>
-                        </View>
-                        <View className="flex flex-row items-center h-[25vh] w-[90vw]">
-                            <FlatList
-                                horizontal
-                                data={DATA}
-                                renderItem={({ item }) => (
-                                    <View className="flex h-[15vh] w-[55vw] mr-[5vw] mb-[2.5vh] border border-solid rounded-[15px]">
-                                        <View className="flex flex-row justify-between items-center ml-[3vw] mr-[4vw] mt-[1vh]">
-                                            <Text className="text-base font-semibold text-sky-700">
-                                                {item.name}
-                                            </Text>
-                                            <Pressable
-                                                onPress={handlePressInfo}
-                                            >
-                                                <AntDesign
-                                                    name="infocirlceo"
-                                                    size={16}
-                                                    color="black"
-                                                />
-                                            </Pressable>
-                                        </View>
-                                        <View className="flex justify-start mx-[3vw] my-[0.5vh]">
-                                            <Text className="text-sky-900 h-[10vh]">
-                                                {item.info}
-                                            </Text>
-                                        </View>
-                                    </View>
-                                )}
-                            />
-                        </View>
+                    <View className="flex justify-center items-center">
+                        <Text className="text-sm font-bold h-[3vh]">
+                            您的過負荷評分為 :{' '}
+                            <Text className={RESULT.style}>{RESULT.level}</Text>
+                        </Text>
+                        <Text className="text-xs">
+                            根據您的過負荷評分，提供以下資訊參考
+                        </Text>
+                        <Text className="text-xs">
+                            若您的過負荷評分風險較高，請儘速尋求專業醫療協助
+                        </Text>
                     </View>
-                    <View className="flex justify-center items-center h-[5vh]">
+                    <View className="flex justify-center items-center h-[10vh]">
                         <Text className="text-xl font-semibold">衛教資訊</Text>
                     </View>
-                    <View className="flex justify-center items-center mt-[5vh]">
+                    <View className="flex justify-center items-center">
                         <View className="flex h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]">
                             <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
                                 <Text className="text-xl font-semibold text-indigo-700">
---
+++ components/RatingBattery.tsx
@@ -0,0 +1,21 @@
+import { View } from 'react-native'
+
+export default function RatingBattery() {
+    return (
+        <View className="flex justify-center items-center w-[35vw]">
+            <View className="flex justify-center items-center w-[21vw] p-[0.75vw] border border-dashed rounded">
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#ff001d]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fd492e]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fc6b25]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fb911c]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#f9b712]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#f8dd08]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#f0f719]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#bdff00]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#8dff00]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#60ff00]"></View>
+                <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#2eff00]"></View>
+            </View>
+        </View>
+    )
+}
