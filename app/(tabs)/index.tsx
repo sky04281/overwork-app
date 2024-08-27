4f02d70b524dc7e29569ed67d6602e218ee40b99
KevinChen <120458845+KevinLaKao@users.noreply.github.com>
2024-08-27 08:28:33 +0000

    每打一行都要通靈想像一下畫面

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,19 +4,22 @@ import {
     SafeAreaView,
     StyleSheet,
     ScrollView,
-    Pressable,
 } from 'react-native'
-import AntDesign from '@expo/vector-icons/AntDesign'
 import { useState } from 'react'
 import InfoModal from '@/components/InfoModal'
-import Battery from '@/components/RatingBattery'
+import RatingBattery from '@/components/RatingBattery'
+import HealthInfo from '@/components/HealthInfo'
 
 export default function HomeScreen() {
     const [infoModalVisible, setInfoModalVisible] = useState(false)
-    const RESULT = { style: 'text-red-500', level: '高風險' }
-    const TITLE = '疾病一'
-    const INFO =
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat alias aspernatur natus architecto non magnam ipsum placeat accusantium harum pariatur, deserunt cupiditate? Voluptate dignissimos laudantium dolorum quidem, repudiandae maiores sint.'
+
+    // 應該要 useState 的 但現在先不能改
+    const overworkScore = 69
+    const lifeScore = 99
+
+    // 開會有提到 低於60 是低風險 其他還不確定
+    const overworkRate = overworkScore > 60 ? ( overworkScore > 80 ? { style: 'text-red-600', level: '高風險' } : { style: 'text-red-400', level: '中風險' } ) : { style: 'text-red-200', level: '低風險' }
+    const lifeRate = lifeScore > 60 ? ( lifeScore > 80 ? { style: 'text-red-600', level: '高風險' } : { style: 'text-red-400', level: '中風險' } ) : { style: 'text-red-200', level: '低風險' }
     const handlePressInfo = () => {
         setInfoModalVisible(true)
     }
@@ -37,22 +40,32 @@ export default function HomeScreen() {
                         </Text>
                     </View>
                     <View className="flex flex-row">
-                        <View className="flex flex-row justify-center items-center h-[35vh] w-[50vw]">
-                            <View className="flex justify-between items-center h-[25vh] w-[10vw] ml-[5vw]">
-                                <Text className="text-red-600">High</Text>
-                                <Text className="text-green-600">Low</Text>
+                        <View>
+                            <View className="flex flex-row justify-center items-center h-[35vh] w-[50vw]">
+                                <View className="flex justify-between items-center h-[25vh] w-[10vw] ml-[5vw]">
+                                    <Text className="text-red-600">High</Text>
+                                    <Text className="text-green-600">Low</Text>
+                                </View>
+                                <RatingBattery score={overworkScore}/>
                             </View>
-                            <Battery score={69}/>
+                            <View className='flex justify-center items-center w-[50vw]'>
+                                <Text className={overworkRate.style}>{overworkRate.level}</Text>
+                            </View>
                         </View>
-                        <View className="h-[35vh] w-[50vw]">
-                            <Text>right</Text>
+                        <View>
+                            <View className="flex flex-row justify-center items-center h-[35vh] w-[50vw]">
+                                <RatingBattery score={lifeScore}/>
+                                <View className="flex justify-between items-center h-[25vh] w-[10vw] mr-[5vw]">
+                                    <Text className="text-red-600">High</Text>
+                                    <Text className="text-green-600">Low</Text>
+                                </View>
+                            </View>
+                            <View className='flex justify-center items-center w-[50vw]'>
+                                <Text className={lifeRate.style}>{lifeRate.level}</Text>
+                            </View>
                         </View>
                     </View>
                     <View className="flex justify-center items-center">
-                        <Text className="text-sm font-bold h-[3vh]">
-                            您的過負荷評分為 :{' '}
-                            <Text className={RESULT.style}>{RESULT.level}</Text>
-                        </Text>
                         <Text className="text-xs">
                             根據您的過負荷評分，提供以下資訊參考
                         </Text>
@@ -63,7 +76,8 @@ export default function HomeScreen() {
                     <View className="flex justify-center items-center h-[10vh]">
                         <Text className="text-xl font-semibold">衛教資訊</Text>
                     </View>
-                    <View className="flex justify-center items-center">
+                    <HealthInfo overworkScore={overworkScore} lifeScore={lifeScore} handlePressInfo={handlePressInfo}/>
+                    {/* <View className="flex justify-center items-center">
                         <View className="flex h-[15vh] w-[80vw] mb-[5vh] mr-[5vh] border border-solid rounded-[15px]">
                             <View className="flex flex-row justify-between items-center mx-[3vw] mt-[1vh]">
                                 <Text className="text-xl font-semibold text-indigo-700">
@@ -132,7 +146,7 @@ export default function HomeScreen() {
                                 <Text className="text-indigo-900">{INFO}</Text>
                             </View>
                         </View>
-                    </View>
+                    </View> */}
                 </View>
                 <InfoModal
                     infoModalVisible={infoModalVisible}
--- components/RatingBattery.tsx
+++ components/RatingBattery.tsx
@@ -1,18 +1,18 @@
 import { View } from 'react-native'
 
-export default function RatingBattery({score} : {score: number}) {
+export default function RatingBattery({ score }: { score: number }) {
     const colorClasses = [
-        " bg-[#ff001d] ",
-        " bg-[#fd492e] ",
-        " bg-[#fc6b25] ",
-        " bg-[#fb911c] ",
-        " bg-[#f9b712] ",
-        " bg-[#f8dd08] ",
-        " bg-[#f0f719] ",
-        " bg-[#bdff00] ",
-        " bg-[#8dff00] ",
-        " bg-[#60ff00] ",
-        " bg-[#2eff00] ",
+        ' bg-[#ff001d] ',
+        ' bg-[#fd492e] ',
+        ' bg-[#fc6b25] ',
+        ' bg-[#fb911c] ',
+        ' bg-[#f9b712] ',
+        ' bg-[#f8dd08] ',
+        ' bg-[#f0f719] ',
+        ' bg-[#bdff00] ',
+        ' bg-[#8dff00] ',
+        ' bg-[#60ff00] ',
+        ' bg-[#2eff00] ',
     ]
 
     // score 將百分制分成 1~11 ( 0~9 -> 1, 10~19 -> 2 ...) 越低越好、越低越綠、越高越紅
@@ -21,23 +21,26 @@ export default function RatingBattery({score} : {score: number}) {
     return (
         <View className="flex justify-center items-center w-[35vw]">
             <View className="flex justify-center items-center w-[21vw] p-[0.75vw] border border-dashed rounded">
-                {
-                    colorClasses.map((color, index) => {
-                        return (
-                            <View className={
+                {colorClasses.map((color, index) => {
+                    return (
+                        <View
+                            className={
                                 index + 1 < classedScore
-                                    ? "h-[2vh] w-[18vw] m-[0.75vw] border rounded opacity-35" + color
-                                    : "h-[2vh] w-[18vw] m-[0.75vw] border rounded" + color
-                            }></View>
-                        )
-                    })
-                }
+                                    ? 'h-[2vh] w-[18vw] m-[0.75vw] border rounded opacity-35' +
+                                      color
+                                    : 'h-[2vh] w-[18vw] m-[0.75vw] border rounded' +
+                                      color
+                            }
+                        ></View>
+                    )
+                })}
             </View>
         </View>
     )
 }
 
-{/* <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#ff001d]"></View>
+{
+    /* <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#ff001d]"></View>
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fd492e]"></View>
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fc6b25]"></View>
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#fb911c]"></View>
@@ -47,4 +50,5 @@ export default function RatingBattery({score} : {score: number}) {
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#bdff00]"></View>
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#8dff00]"></View>
 <View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#60ff00]"></View>
-<View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#2eff00]"></View> */}
+<View className="h-[2vh] w-[18vw] m-[0.75vw] border rounded bg-[#2eff00]"></View> */
+}
